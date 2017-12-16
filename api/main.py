62c682c3e0235c8787ffebb67f9ff724b7fd2131
Scott Miao <scott_miao@trend.com.tw>
2017-12-16 13:30:01 +0000

    add sleep and secret api

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import time
 from bottle import get, run
 
 logging.basicConfig()
@@ -8,6 +9,21 @@ __logger = logging.getLogger(__name__)
 @get('/healthcheck')
 def healthcheck():
     return "Hello World!"
+
+@get('/sleep/<secs>')
+def sleep(secs=20):
+    time.sleep(secs)
+    return 'sleep for {} secs'.format(secs)
+
+@get('/secret')
+def get_secret():
+    import os
+    p = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf', 'secret.txt')
+    secret = None
+    with open(p, 'rb') as f:
+        secret = f.read()
+    return secret
+
 
 @get('/')
 def welcome():
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,9 @@
+import os
 import unittest
 
-from api.main import healthcheck, welcome
+import datetime
+
+from api.main import healthcheck, welcome, sleep, get_secret
 
 
 class MainTest(unittest.TestCase):
@@ -10,3 +13,20 @@ class MainTest(unittest.TestCase):
 
     def test_welcome(self):
         self.assertEqual('Welcome to my home', welcome())
+
+    def test_sleep(self):
+        secs = 5
+        st = datetime.datetime.utcnow()
+        msg = sleep(secs)
+        et = datetime.datetime.utcnow()
+        tdiff = et - st
+        self.assertTrue(tdiff.seconds >= 5)
+        self.assertEqual('sleep for {} secs'.format(secs), msg)
+
+    def test_get_secret(self):
+        secret = get_secret()
+        p = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../api/conf', 'secret.txt')
+        with open(p, 'rb') as f:
+            expected = f.read()
+        self.assertEqual(expected, secret)
+
